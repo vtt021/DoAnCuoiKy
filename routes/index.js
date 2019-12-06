b67a17c9bc86f8b7d5bf88e9780a3bb0ac68498b
vtt021 <57207504+vtt021@users.noreply.github.com>
2019-12-06 19:07:43 +0700

    Sua Procfile

--- routes/index.js
+++ routes/index.js
@@ -3,42 +3,7 @@ var router = express.Router();
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-/*
 // Connection URL
-const url = 'mongodb+srv://thientin:12345679@cluster0-yf6sd.mongodb.net/test?retryWrites=true&w=majority'||'mongodb://localhost:27017';
-// Database Name
-const dbName = 'QuanLySanPham';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-  router.get('/', function (req, res, next) {
-    var collection = db.collection('Products');
-    var data2 = Array;
-    collection.find({}).toArray(function (e, docs) {
-      console.log(docs);
-      data2 = docs;
-    });
-    collection.find({}).toArray(function (e, docs) {
-      router.get('/', function (req, res, next) {
-        res.render('index', {
-          "data": docs,
-          "data2": data2
-        });
-      });
-    });
-  });
-});
-
-*/
-
-
 const uri = 'mongodb+srv://thientin:12345679@cluster0-yf6sd.mongodb.net/test?retryWrites=true&w=majority';
 //'mongodb+srv://thientin:12345679@cluster0-yf6sd.mongodb.net/test?retryWrites=true&w=majority' ||
 //'mongodb://localhost:27017'
@@ -52,8 +17,6 @@ client.connect(err => {
   var data2 = Array;
   collection.find({}).toArray(function (e, docs) {
     data2 = docs;
-    console.log(docs);
-    console.log("done");
     router.get('/', function (req, res, next) {
       res.render('index', {
         "data": data2
