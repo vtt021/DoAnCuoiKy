7f6b39f34871902d45241a7bc864c14ab58ba989
vtt021 <57207504+vtt021@users.noreply.github.com>
2020-01-05 17:51:04 +0700

    update MVC bread, pet, vegetanle, pet

--- routes/vegetables.js
+++ routes/vegetables.js
@@ -1,28 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-/* GET vegetables page */
 
-const uri = 'mongodb+srv://thientin:12345679@cluster0-yf6sd.mongodb.net/test?retryWrites=true&w=majority' || 'mongodb://localhost:27017';
-//'mongodb+srv://thientin:12345679@cluster0-yf6sd.mongodb.net/test?retryWrites=true&w=majority' ||
-//'mongodb://localhost:27017'
-const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
-const dbName = 'QuanLySanPham';
-var data = Array;
-router.get('/', function(req, res, next) {
-    client.connect(err => {
-        assert.equal(null, err);
-        console.log("Connected successfully to server");
-        const db = client.db(dbName);
-        var collection = db.collection('Vegetables');
-        collection.find({}).toArray(function(e, docs) {
-            data = docs;
-        });
-        res.render('vegetables', { data: data });
-        // perform actions on the collection object
-        client.close();
-    });
+const productController = require('../controller/productController')
 
-});
+router.get('/', productController.getVegetable);
+
 module.exports = router;
